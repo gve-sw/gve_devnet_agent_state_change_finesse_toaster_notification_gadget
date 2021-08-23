64339b9986188ea2a68f2ad392f96b681a7efacc
GVE Devnet Admin <gve-devnet.gen@cisco.com>
2021-08-23 20:15:47 +0000

    Baselined from internal Repository last_commit:bb1976de4aadd1e94c6ed236a4b2eb1398a6713f

--- ToasterNotification/ToasterNotification.js
+++ ToasterNotification/ToasterNotification.js
@@ -32,117 +32,7 @@ finesse.modules = finesse.modules || {};
 finesse.modules.ToasterNotificationGadget = (function ($) {
     var user, states, dialogs, clientlogs,
 
-    /**
-     * Populates the fields in the gadget with data
-     */
-    render = function () {
-        var currentState = user.getState();
 
-        // Examples of getting data from the User object (GET)
-        $("#userId").text(user.getId());
-        $("#firstName").text(user.getFirstName());
-        $("#lastName").text(user.getLastName());
-        if (user.hasSupervisorRole()) {
-            $("#userRole").text('Supervisor');
-        } else {
-            $("#userRole").text('Agent');
-        }
-        $("#extension").text(user.getExtension());
-        $("#userState").text(currentState);
-
-        // Example of setting the user state (PUT)
-        if (currentState === states.NOT_READY) {
-            $("#goReady").show();
-            $("#goNotReady").hide();
-        } else if (currentState === states.READY) {
-            $("#goNotReady").show();
-            $("#goReady").hide();
-        } else {
-            $("#goNotReady").hide();
-            $("#goReady").hide();
-        }
-        
-        gadgets.window.adjustHeight();
-    },
-
-    displayCall = function (dialog) {
-        var callVars = dialog.getMediaProperties();
-
-        // Examples of getting data from the Dialog object (GET)
-        $("#dnis").text(dialog.getMediaProperties().DNIS);
-        $("#callType").text(dialog.getMediaProperties().callType);
-        $("#fromAddress").text(dialog.getFromAddress());
-        $("#toAddress").text(dialog.getToAddress());
-        $("#callState").text(dialog.getState());
-
-        // Hide the make call button when the user is on a call
-        $("#makeCallButton").hide();
-
-        // Example of using data from the dialog to do a web search
-        $("#bing").attr("src","https://www.bing.com/search?q=" + callVars["callVariable3"]);
-    },
-
-    _processCall = function (dialog) {
-        displayCall(dialog);
-    },
-
-    /**
-     *  Handler for additions to the Dialogs collection object. This will occur when a new
-     *  Dialog is created on the Finesse server for this user.
-     */
-    handleNewDialog = function(dialog) {
-        // call the displayCall handler
-        displayCall(dialog);
-
-        // add a dialog change handler in case the callvars didn't arrive yet
-        dialog.addHandler('change', _processCall);
-    },
-     
-    /**
-     *  Handler for deletions from the Dialogs collection object for this user. This will occur
-     *  when a Dialog is removed from this user's collection (example, end call)
-     */
-    handleEndDialog = function(dialog) {
-        // Clear the fields when the call is ended
-        $("#callId").text("");
-        $("#dnis").text("");
-        $("#callType").text("");
-        $("#fromAddress").text("");
-        $("#toAddress").text("");
-        $("#callState").text("");
-
-        // Show the make call button when the call is ended
-        $("#makeCallButton").show();
-
-        // Remove the dialog data from the web search
-        $("#bing").attr("src","https://www.bing.com");
-    },
-
-    /**
-     * Handler for makeCall when successful.
-     */
-    makeCallSuccess = function(rsp) { },
-    
-    /**
-     * Handler for makeCall when error occurs.
-     */
-    makeCallError = function(rsp) { },
-
-    /**
-     * Handler for the onLoad of a User object. This occurs when the User object is initially read
-     * from the Finesse server. Any once only initialization should be done within this function.
-     */
-    handleUserLoad = function (userevent) {
-        // Get an instance of the dialogs collection and register handlers for dialog additions and
-        // removals
-        dialogs = user.getDialogs( {
-            onCollectionAdd : handleNewDialog,
-            onCollectionDelete : handleEndDialog
-        });
-
-        render();
-    },
-      
     /**
      *  Handler for all User updates
      */
@@ -163,38 +53,10 @@ finesse.modules.ToasterNotificationGadget = (function ($) {
                 );
             }
         }
-        render();
     };
 
     /** @scope finesse.modules.ToasterNotificationGadget */
     return {
-        /**
-         * Sets the user state
-         */
-        setUserState : function (state) {
-            clientLogs.log("setUserState(): The user's current state is: " + state);
-            if (state === 'READY') {
-                user.setState(states.READY);
-            } else if (state === 'NOT_READY') {
-                user.setState(states.NOT_READY);
-            }
-        },
-
-        /**
-         * Make a call to the number
-         */
-        makeCall : function (number) {
-            clientLogs.log("makeCall(): Making a call to " + number);
-            // Example of the user make call method
-            user.makeCall(number, {
-                success: makeCallSuccess,
-                error: makeCallError
-            });
-
-            // Hide the button after making the call
-            $("#makeCallButton").hide();
-        },
-
         /**
          * Performs all initialization for this gadget
          */
@@ -218,7 +80,6 @@ finesse.modules.ToasterNotificationGadget = (function ($) {
 
             user = new finesse.restservices.User({
                 id: cfg.id, 
-                onLoad : handleUserLoad,
                 onChange : handleUserChange
             });
 
@@ -234,7 +95,6 @@ finesse.modules.ToasterNotificationGadget = (function ($) {
                 gadgets.window.adjustHeight();
             });
             containerServices.makeActiveTabReq();
-            //containerServices.hideDialog();
         }
     };
 }(jQuery));
